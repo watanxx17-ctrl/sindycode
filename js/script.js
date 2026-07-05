33466edfac177c398e58c7c2d8cb83edfd5a5af5
watanxx17-ctrl <watanxx.17@gmail.com>
2026-07-06 01:33:47 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,472 @@
+const guns = [
+            {
+                name: "M250",
+                type: "Light Machine Gun",
+                code: "M250 General Machine Gun-Operations-6KIK8P400UQCIBP788D1T",
+                imageUrl: "img/m2501.jpg" 
+            },
+            {
+                name: "M250",
+                type: "Light Machine Gun",
+                code: "M250 General Machine Gun-Operations-6KIK8QC00UQCIBP788D1T",
+                imageUrl: "img/m2502.jpg" 
+            },
+            {
+                name: "M250",
+                type: "Light Machine Gun",
+                code: "M250 General Machine Gun-Operations-6KIK8R400UQCIBP788D1T",
+                imageUrl: "img/m2503.jpg" 
+            },
+            {
+                name: "M250",
+                type: "Light Machine Gun",
+                code: "M250 General Machine Gun-Operations-6KIK8RG00UQCIBP788D1T",
+                imageUrl: "img/m2504.jpg" 
+            },
+            {
+                name: "M250",
+                type: "Light Machine Gun",
+                code: "M250 General Machine Gun-Operations-6KIK8RO00UQCIBP788D1T",
+                imageUrl: "img/m2505.jpg" 
+            },
+			{
+                name: "QJB โต่วอิน",
+                type: "Light Machine Gun",
+                code: "QJB-201 Light Machine Gun-Operations-6KIK8TK00UQCIBP788D1T",
+                imageUrl: "img/qjb1.jpg" 
+            },
+			{
+                name: "QJB",
+                type: "Light Machine Gun",
+                code: "QJB-201 Light Machine Gun-Operations-6KIK94800UQCIBP788D1T",
+                imageUrl: "img/qjb2.jpg" 
+            },
+			{
+                name: "QJB",
+                type: "Light Machine Gun",
+                code: "QJB-201 Light Machine Gun-Operations-6KIK94O00UQCIBP788D1T",
+                imageUrl: "img/qjb3.jpg" 
+            },
+			{
+                name: "QJB",
+                type: "Light Machine Gun",
+                code: "QJB-201 Light Machine Gun-Operations-6KIK95000UQCIBP788D1T",
+                imageUrl: "img/qjb4.jpg" 
+            },
+            {
+                name: "M249",
+                type: "Light Machine Gun",
+                code: "M249 Light Machine Gun-Operations-6KIK98C00UQCIBP788D1T", 
+                imageUrl: "img/m249.jpg"
+            },
+            {
+                name: "PKM",
+                type: "Light Machine Gun",
+                code: "PKM General Machine Gun-Operations-6KIK9IO00UQCIBP788D1T", 
+                imageUrl: "img/pkm1.jpg"
+            },
+            {
+                name: "PKM",
+                type: "Light Machine Gun",
+                code: "PKM General Machine Gun-Operations-6KIK9J400UQCIBP788D1T", 
+                imageUrl: "img/pkm2.jpg"
+            },
+            {
+                name: "PKM",
+                type: "Light Machine Gun",
+                code: "PKM General Machine Gun-Operations-6KIK9JC00UQCIBP788D1T", 
+                imageUrl: "img/pkm3.jpg"
+            },
+            {
+                name: "PKM",
+                type: "Light Machine Gun",
+                code: "PKM General Machine Gun-Operations-6KIK9K800UQCIBP788D1T", 
+                imageUrl: "img/pkm4.jpg"
+            },
+            {
+                name: "MK47",
+                type: "Assault Rifle",
+                code: "MK47 Assault Rifle-Operations-6KIKAB800UQCIBP788D1T", 
+                imageUrl: "img/mk471.jpg"
+            },
+			{
+                name: "MK47",
+                type: "Assault Rifle",
+                code: "MK47 Assault Rifle-Operations-6KIKAC000UQCIBP788D1T", 
+                imageUrl: "img/mk472.jpg"
+            },
+			{
+                name: "MK47",
+                type: "Assault Rifle",
+                code: "MK47 Assault Rifle-Operations-6K5CB4800UQCIBP788D1T", 
+                imageUrl: "img/mk473.jpg"
+            },
+            {
+                name: "KC17",
+                type: "Assault Rifle",
+                code: "KC17 Assault Rifle-Operations-6KIKATG00UQCIBP788D1T", 
+                imageUrl: "img/kc171.jpg"
+            },
+			{
+                name: "KC17",
+                type: "Assault Rifle",
+                code: "KC17 Assault Rifle-Operations-6KIKAV000UQCIBP788D1T", 
+                imageUrl: "img/kc172.jpg"
+            },
+			{
+                name: "KC17",
+                type: "Assault Rifle",
+                code: "KC17 Assault Rifle-Operations-6K5CB9800UQCIBP788D1T", 
+                imageUrl: "img/kc173.jpg"
+            },
+            {
+                name: "K437",
+                type: "Assault Rifle",
+                code: "K437 Assault Rifle-Operations-6KIKBC000UQCIBP788D1T", 
+                imageUrl: "img/k4371.jpg"
+            },
+			{
+                name: "K437",
+                type: "Assault Rifle",
+                code: "K437 Assault Rifle-Operations-6KIKBDG00UQCIBP788D1T", 
+                imageUrl: "img/k4372.jpg"
+            },
+			{
+                name: "K437",
+                type: "Assault Rifle",
+                code: "K437 Assault Rifle-Operations-6KIKBE000UQCIBP788D1T", 
+                imageUrl: "img/k4373.jpg"
+            },
+			{
+                name: "K437",
+                type: "Assault Rifle",
+                code: "K437 Assault Rifle-Operations-6KIKBE800UQCIBP788D1T", 
+                imageUrl: "img/k4374.jpg"
+            },
+			{
+                name: "K437",
+                type: "Assault Rifle",
+                code: "K437 Assault Rifle-Operations-6KIKBEG00UQCIBP788D1T", 
+                imageUrl: "img/k4375.jpg"
+            },
+            {
+                name: "CI-19",
+                type: "Assault Rifle",
+                code: "CI-19 Assault Rifle-Operations-6KIKC2O00UQCIBP788D1T", 
+                imageUrl: "img/ci1.jpg"
+            },
+			{
+                name: "CI-19",
+                type: "Assault Rifle",
+                code: "CI-19 Assault Rifle-Operations-6KIKC4C00UQCIBP788D1T", 
+                imageUrl: "img/ci2.jpg"
+            },
+            {
+                name: "M7",
+                type: "Assault Rifle",
+                code: "M7 Battle Rifle-Operations-6KIK6TC00UQCIBP788D1T", 
+                imageUrl: "img/m71.jpg"
+            },
+            {
+                name: "M7",
+                type: "Assault Rifle",
+                code: "M7 Battle Rifle-Operations-6KIK6UC00UQCIBP788D1T", 
+                imageUrl: "img/m72.jpg"
+            },
+            {
+                name: "M7 โต่วอิน",
+                type: "Assault Rifle",
+                code: "M7 Battle Rifle-Operations-6JQUI2O00UQCIBP788D1T", 
+                imageUrl: "img/m73.jpg"
+            },
+            {
+                name: "M7",
+                type: "Assault Rifle",
+                code: "M7 Battle Rifle-Operations-6KIK70000UQCIBP788D1T", 
+                imageUrl: "img/m74.jpg"
+            },
+            {
+                name: "M7",
+                type: "Assault Rifle",
+                code: "M7 Battle Rifle-Operations-6KIK71K00UQCIBP788D1T", 
+                imageUrl: "img/m75.jpg"
+            },
+			{
+                name: "As-val",
+                type: "Assault Rifle",
+                code: "AS Val Assault Rifle-Operations-6KIKD8K00UQCIBP788D1T", 
+                imageUrl: "img/as1.jpg"
+            },
+			{
+                name: "As-val",
+                type: "Assault Rifle",
+                code: "AS Val Assault Rifle-Operations-6KIKD9400UQCIBP788D1T", 
+                imageUrl: "img/as2.jpg"
+            },
+			{
+                name: "As-val",
+                type: "Assault Rifle",
+                code: "AS Val Assault Rifle-Operations-6KIKD9C00UQCIBP788D1T", 
+                imageUrl: "img/as3.jpg"
+            },
+			{
+                name: "As-val",
+                type: "Assault Rifle",
+                code: "AS Val Assault Rifle-Operations-6KIKD9G00UQCIBP788D1T", 
+                imageUrl: "img/as4.jpg"
+            },
+			{
+                name: "Ar-57",
+                type: "Assault Rifle",
+                code: "AR-57 Assault Rifle-Operations-6JPOG7000UQCIBP788D1T", 
+                imageUrl: "img/ar571.jpg"
+            },
+			{
+                name: "Ar-57",
+                type: "Assault Rifle",
+                code: "AR-57 Assault Rifle-Operations-6KIKA1400UQCIBP788D1T", 
+                imageUrl: "img/ar572.jpg"
+            },
+			{
+                name: "RM277",
+                type: "Assault Rifle",
+                code: "RM277-Operations-6KIK9RC00UQCIBP788D1T", 
+                imageUrl: "img/rm1.jpg"
+            },
+            {
+                name: "AUG",
+                type: "Assault Rifle",
+                code: "AUG Assault Rifle-Operations-6KIKDUS00UQCIBP788D1T", 
+                imageUrl: "img/aug1.jpg"
+            },
+            {
+                name: "AUG",
+                type: "Assault Rifle",
+                code: "AUG Assault Rifle-Operations-6KIKDV800UQCIBP788D1T", 
+                imageUrl: "img/aug2.jpg"
+            },
+			{
+                name: "AUG",
+                type: "Assault Rifle",
+                code: "AUG Assault Rifle-Operations-6KIKDVG00UQCIBP788D1T", 
+                imageUrl: "img/aug3.jpg"
+            },
+            {
+                name: "K416",
+                type: "Assault Rifle",
+                code: "K416 Assault Rifle-Operations-6KIKE9O00UQCIBP788D1T", 
+                imageUrl: "img/k4161.jpg"
+            },
+			{
+                name: "K416",
+                type: "Assault Rifle",
+                code: "K416 Assault Rifle-Operations-6KIKEA400UQCIBP788D1T", 
+                imageUrl: "img/k4162.jpg"
+            },
+			{
+                name: "K416",
+                type: "Assault Rifle",
+                code: "K416 Assault Rifle-Operations-6KIKEA800UQCIBP788D1T", 
+                imageUrl: "img/k4163.jpg"
+            },
+            {
+                name: "ASh-12",
+                type: "Assault Rifle",
+                code: "ASh-12 Assault Rifle-Operations-6KIKEIO00UQCIBP788D1T", 
+                imageUrl: "img/ash1.jpg"
+            },
+			{
+                name: "ASh-12",
+                type: "Assault Rifle",
+                code: "ASh-12 Assault Rifle-Operations-6KIKEJ000UQCIBP788D1T", 
+                imageUrl: "img/ash2.jpg"
+            },
+			{
+                name: "ASh-12",
+                type: "Assault Rifle",
+                code: "ASh-12 Assault Rifle-Operations-6KIKEJ800UQCIBP788D1T", 
+                imageUrl: "img/ash3.jpg"
+            },
+			{
+                name: "Akm",
+                type: "Assault Rifle",
+                code: "AKM Assault Rifle-Operations-6KIKET400UQCIBP788D1T", 
+                imageUrl: "img/akm1.jpg"
+            },
+			{
+                name: "Akm",
+                type: "Assault Rifle",
+                code: "AKM Assault Rifle-Operations-6K458FO00UQCIBP788D1T", 
+                imageUrl: "img/akm2.jpg"
+            },
+			{
+                name: "M4A1",
+                type: "Assault Rifle",
+                code: "M4A1 Assault Rifle-Operations-6KIKF4800UQCIBP788D1T", 
+                imageUrl: "img/m4.jpg"
+            },
+			{
+                name: "G3",
+                type: "Assault Rifle",
+                code: "G3 Battle Rifle-Operations-6KIKDO400UQCIBP788D1T", 
+                imageUrl: "img/g3.jpg"
+            },
+			{
+                name: "MK4",
+                type: "SMG",
+                code: "MK4 Submachine Gun-Operations-6KIKFD800UQCIBP788D1T", 
+                imageUrl: "img/mk4.jpg"
+            },
+			{
+                name: "SR-3M",
+                type: "SMG",
+                code: "SR-3M Compact Assault Rifle-Operations-6KIKFKG00UQCIBP788D1T", 
+                imageUrl: "img/sr3m.jpg"
+            },
+            {
+                name: "SR-25",
+                type: "Marksman Rifle",
+                code: "SR-25 Marksman Rifle-Operations-6KIKG8S00UQCIBP788D1T", 
+                imageUrl: "img/sr25.jpg"
+            },
+			{
+                name: "SVCH",
+                type: "Marksman Rifle",
+                code: "SVCH Marksman Rifle-Operations-6KIKFV80B8CV3THHRDM51", 
+                imageUrl: "img/svch1.jpg"
+            },
+            {
+                name: "M14",
+                type: "Marksman Rifle",
+                code: "M14 Marksman Rifle-Operations-6KIKGFS00UQCIBP788D1T", 
+                imageUrl: "img/m141.jpg"
+            },
+            {
+                name: "M14",
+                type: "Marksman Rifle",
+                code: "M14 Marksman Rifle-Operations-6KIKGG800UQCIBP788D1T", 
+                imageUrl: "img/m142.jpg"
+            },
+            {
+                name: "M14",
+                type: "Marksman Rifle",
+                code: "M14 Marksman Rifle-Operations-6KIKGGG00UQCIBP788D1T", 
+                imageUrl: "img/m143.jpg"
+            },
+			{
+                name: "M14",
+                type: "Marksman Rifle",
+                code: "M14 Marksman Rifle-Operations-6KIKGGO00UQCIBP788D1T", 
+                imageUrl: "img/m144.jpg"
+            },
+            {
+                name: "AWM",
+                type: "Sniper Rifle",
+                code: "AWM Sniper Rifle-Operations-6KIKHAK00UQCIBP788D1T", 
+                imageUrl: "img/awm.jpg"
+            },
+            {
+                name: "IPAD PRO M4",
+                type: "HUD",
+                code: "6b42-ada6-3e9a-3f77-e6dd", 
+                imageUrl: "img/hudipad.jpg"
+            },
+
+        ];
+
+const gunListElement = document.getElementById('gun-list');
+const filterButtonsElement = document.getElementById('filter-buttons');
+let currentFilterType = 'All';
+
+function renderGuns(gunArray) {
+    gunListElement.innerHTML = '';
+    gunArray.forEach((gun, index) => {
+        const uniqueId = gun.name.replace(/\s/g, '-') + '-' + index;
+        const card = document.createElement('div');
+        card.className = 'gun-card';
+        card.setAttribute('data-gun-name', gun.name.toLowerCase());
+
+        card.innerHTML = `
+            <div class="gun-image-container">
+                <img src="${gun.imageUrl}" alt="${gun.name}" 
+                     onerror="this.onerror=null;this.src='https://via.placeholder.com/200x100?text=No+Image';">
+            </div>
+            <div class="gun-info">
+                <h2>${gun.name.toUpperCase()}</h2>
+                <p>${gun.type}</p>
+                <div class="code-display hidden" id="code-box-${uniqueId}" data-code="${gun.code}">${gun.code}</div>
+                <div class="button-group">
+                    <button class="btn btn-show" id="show-btn-${uniqueId}" onclick="toggleCode('${uniqueId}', this)">Show Code</button>
+                    <button class="btn btn-copy" onclick="copyCode('${uniqueId}', this)">Copy</button>
+                </div>
+                <p class="copy-success hidden" id="success-msg-${uniqueId}">คัดลอกสำเร็จ!</p>
+            </div>
+        `;
+        gunListElement.appendChild(card);
+    });
+}
+
+function createFilterButtons() {
+    const types = ['All'];
+    guns.forEach(gun => { if (!types.includes(gun.type)) types.push(gun.type); });
+    filterButtonsElement.innerHTML = '';
+    types.forEach(type => {
+        const button = document.createElement('button');
+        button.className = 'filter-btn';
+        if (type === 'All') button.classList.add('active');
+        button.textContent = type;
+        button.setAttribute('onclick', `filterByType('${type}', this)`);
+        filterButtonsElement.appendChild(button);
+    });
+}
+
+function filterByType(type, buttonElement) {
+    currentFilterType = type;
+    document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
+    buttonElement.classList.add('active');
+    document.getElementById('search-input').value = '';
+    renderGuns(type === 'All' ? guns : guns.filter(gun => gun.type === type));
+}
+
+function filterGuns() {
+    const filterText = document.getElementById('search-input').value.toLowerCase();
+    const filteredByText = guns.filter(gun => gun.name.toLowerCase().includes(filterText) || gun.type.toLowerCase().includes(filterText));
+    document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
+    currentFilterType = 'Search';
+    renderGuns(filteredByText);
+}
+
+function toggleCode(uniqueId, buttonElement) {
+    const codeBox = document.getElementById(`code-box-${uniqueId}`);
+    const isHidden = codeBox.classList.contains('hidden');
+    codeBox.classList.toggle('hidden');
+    buttonElement.textContent = isHidden ? 'Hide Code' : 'Show Code';
+    document.getElementById(`success-msg-${uniqueId}`).classList.add('hidden');
+}
+
+function copyCode(uniqueId, buttonElement) {
+    const codeBox = document.getElementById(`code-box-${uniqueId}`);
+    const code = codeBox.getAttribute('data-code');
+    const successMsg = document.getElementById(`success-msg-${uniqueId}`);
+    const showButton = document.getElementById(`show-btn-${uniqueId}`);
+    if (codeBox.classList.contains('hidden')) {
+        codeBox.classList.remove('hidden');
+        showButton.textContent = 'Hide Code';
+    }
+    navigator.clipboard.writeText(code).then(() => {
+        successMsg.classList.remove('hidden');
+        buttonElement.textContent = 'COPIED! ✅';
+        setTimeout(() => {
+            successMsg.classList.add('hidden');
+            buttonElement.textContent = 'Copy';
+        }, 3000);
+    }).catch(err => {
+        console.error('ไม่สามารถคัดลอกได้:', err);
+        alert('ไม่สามารถคัดลอกได้ โปรดลองคัดลอกด้วยตนเอง');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    createFilterButtons();
+    renderGuns(guns);
+});
